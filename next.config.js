2e72fe19194b3dff8ce4e89e90eb567e7f5b54b6
selmanahmad57-a11y <selmanahmad57@gmail.com>
2025-08-23 14:12:32 +0000

    Remove appDir from next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // The appDir is enabled by default in App Router
+  // No need to specify it in experimental anymore
+  // other config options...
+}
+
+module.exports = nextConfig
